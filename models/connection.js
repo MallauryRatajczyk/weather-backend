f3d1cb5774f644fb127bf3ae8bc115155f7e0aa3
Mallaury <mallaury.ratajczyk@gmail.com>
2024-09-20 17:48:14 +0200

    unuse .env

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = process.env.CONNECTION_STRING;
+const connectionString = 'mongodb+srv://mallauryratajczyk:ilTUExVpw99oCN4n@cluster0.gi6g6.mongodb.net/weather';
 
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
